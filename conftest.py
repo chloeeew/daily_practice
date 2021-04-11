b37681758d2f8d3b260f9f66ef40aa7fe645aecc
ChloeeeeWang <403505960@qq.com>
2021-04-11 11:54:11 +0800

    conftest ---- encode pytest collect item to utf-8

---
+++ conftest.py
@@ -0,0 +1,16 @@
+"""
+==================
+Author:Chloeee
+Time:2021/4/11 11:46
+Contact:403505960@qq.com
+==================
+"""
+
+import pytest
+
+from typing import List
+def pytest_collection_modifyitems(items):
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+
