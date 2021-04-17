fca55a6010b1bec0256fcef8d4cbe1dccbf711a1
ChloeeeeWang <403505960@qq.com>
2021-04-17 16:24:01 +0800

    add yaml_controller.py in common file and calculator.py in pages file

--- test_cal.py
+++ test_cal.py
@@ -9,12 +9,13 @@ Contact:403505960@qq.com
 from data.calculator import data_add_int,data_divide,data_add_float,ids_add_int,ids_divide,ids_add_float
 from pages.calculator import Calculator
 import pytest
+import allure
 
 
 
 
 
-
+@allure.feature("测试计算器")
 @pytest.mark.usefixtures("calculate_func")
 class TestCalculator:
     # def setup(self):
@@ -28,6 +29,7 @@ class TestCalculator:
 
     # @pytest.mark.parametrize("a,b,expect", data_add_int, ids=ids_add_int)
     # 使用fixture参数
+    @allure.story("测试int情况")
     @pytest.mark.run(order=2)
     def test_answer_add_int(self,prepare_add_int):
         """测试int的情况"""
