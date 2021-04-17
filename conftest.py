747741626e6643de3e988d8a5dcdf42bfeb9a138
ChloeeeeWang <403505960@qq.com>
2021-04-17 12:34:44 +0800

    1--add command line parameters 2--use fixture instead of setup and teardown

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ Contact:403505960@qq.com
 ==================
 """
 
+from data.calculator import data_add_int,ids_add_int
 import pytest
 from typing import List
 
@@ -13,4 +14,38 @@ def pytest_collection_modifyitems(items):
     for item in items:
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+
+
+# 添加命令行参数 --env 切换环境测试数据
+def pytest_addoption(parser):
+    parser.addoption("--env", action="store",
+                     default="release",
+                     help="根据环境切换测试数据，release==测试环境//master==业务环境")
+
+@pytest.fixture(scope='session',autouse=True)
+def envir(request):
+    myenv = request.config.getoption("--env")
+    if myenv == "release":
+        print("测试环境数据")
+    elif myenv == 'master':
+        print("业务环境测试数据")
+    else:
+        print('没有测试数据')
+
+
+
+@pytest.fixture()
+def calculate_func():
+    print("-------------")
+    print("开始计算")
+    yield
+    print('计算结束')
+    print("-------------")
+
+
+@pytest.fixture(params=data_add_int,ids=ids_add_int)
+def prepare_add_int(request):
+    yield request.param
+
+
 
--- test_cal.py
+++ test_cal.py
@@ -7,63 +7,47 @@ Contact:403505960@qq.com
 """
 
 from data.calculator import data_add_int,data_divide,data_add_float,ids_add_int,ids_divide,ids_add_float
+from pages.calculator import Calculator
 import pytest
 
 
 
 
 
-def func_divide(a, b):
-    try:
-        result = round(a / b, 2)
-    except ZeroDivisionError as e:
-        return 'error'
-    except TypeError as e:
-        return 'error'
-    else:
-        return result
 
-
-def func_multiply(a, b):
-    return a * b
-
-
-def func_minus(a, b):
-    return a - b
-
-
-def func_add(a, b):
-    return round(a + b, 2)
-
-
+@pytest.mark.usefixtures("calculate_func")
 class TestCalculator:
-    def setup(self):
-        print("-------------")
-        print("开始计算")
+    # def setup(self):
+    #     print("-------------")
+    #     print("开始计算")
 
-    def teardown(self):
-        print("\n 结束计算 \n")
-        print("------------")
+    # def teardown(self):
+    #     print("\n 结束计算 \n")
+    #     print("------------")
 
-    @pytest.mark.parametrize("a,b,expect", data_add_int, ids=ids_add_int)
-    def test_answer_add_int(self,a,b,expect):
+
+    # @pytest.mark.parametrize("a,b,expect", data_add_int, ids=ids_add_int)
+    # 使用fixture参数
+    @pytest.mark.run(order=2)
+    def test_answer_add_int(self,prepare_add_int):
         """测试int的情况"""
-        assert expect == func_add(a,b)
+        a, b, expect = prepare_add_int
+        assert expect == Calculator.func_add(a,b)
 
+    @pytest.mark.run(order=1)
     @pytest.mark.parametrize("a,b,expect", data_add_float, ids=ids_add_float)
     def test_answer_add_float(self,a,b,expect):
         """测试float的情况"""
-        assert expect == func_add(a, b)
-
-    def test_answer_minus(self):
-        assert 4 == func_minus(10,6)
-
-    def test_answer_multiply(self):
-        assert 9 == func_multiply(3,3)
+        assert expect == Calculator.func_add(a, b)
 
     @pytest.mark.parametrize("a,b,expect", data_divide, ids=ids_divide)
     def test_answer_divide(self,a,b,expect):
-        assert expect == func_divide(a,b)
+        assert expect == Calculator.func_divide(a,b)
 
+    def test_answer_minus(self):
+        assert 4 == Calculator.func_minus(10,6)
+
+    def test_answer_multiply(self):
+        assert 9 == Calculator.func_multiply(3,3)
 
 
